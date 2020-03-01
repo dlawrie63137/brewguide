69baef828fc69aa4ba0bb710bd2b71f495fb4a2d
David Lawrie <dlawrie@att.net>
2020-02-29 23:12:32 -0600

    tweaked navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,16 +4,18 @@ import mblogo from '../../assets/img/mblogo_long_wd.png';
 
 const navbar = () => {
     return (
-        <nav className="navbar navbar-light bg-light">
-           <a className="navbar-brand" href="#">
-             <img src={ mblogo }  className="d-inline-block align-top" alt="" />
-                <ul className="nav_ul">
-                    <li><a href='#'>Find a Brewery</a></li>
-                    <li><a href='#'>About MongoByte</a></li>
-                    <li><a href='#'>Contact Us</a></li>
-                </ul> 
-  </a>
-</nav>
+        <div className='nav_nav'>
+            <nav className="navbar navbar-light bg-light">
+              {/* <a className="navbar-brand" href="#"> */}
+                 <img src={ mblogo }  className="d-inline-block align-top" alt="" />
+                    <ul className="nav_ul">
+                        <li><a href='#'>Find a Brewery</a></li>
+                        <li><a href='#'>About MongoByte</a></li>
+                       <li><a href='#'>Contact Us</a></li>
+                    </ul> 
+              {/*  </a> */}
+            </nav>
+        </div>
     );
 };
 
