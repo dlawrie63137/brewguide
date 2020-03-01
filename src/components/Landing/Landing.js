0eca205d3db1ce2c6620ceaf173524271a010947
David Lawrie <dlawrie@att.net>
2020-02-29 20:45:50 -0600

    Navbar

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const landing = ( props ) => {
+    return (
+        <div>
+           <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default landing;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css';
+import mblogo from '../../assets/img/mblogo_long_wd.png';
+
+const navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+           <a className="navbar-brand" href="#">
+             <img src={ mblogo }  className="d-inline-block align-top" alt="" />
+                <ul className="nav_ul">
+                    <li><a href='#'>Find a Brewery</a></li>
+                    <li><a href='#'>About MongoByte</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+                </ul> 
+  </a>
+</nav>
+    );
+};
+
+export default navbar;
